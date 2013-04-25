ef8504a941b036366bf1a05dda6f6da253c491c2
sijokg <cijokgeorge@yahoo.co.in>
2013-04-25 22:11:01 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eco::Application.config.secret_key_base = 'cf411bb7f136dcbff58054efc55b7f2df982c07254841d89fc7df21d51ed967eef8b0a23399f24d5a0366f9873148d352bb785e1031c0fbe9729b66699e77040'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eco::Application.config.session_store :encrypted_cookie_store, key: '_eco_session'
