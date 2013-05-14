92554aaa1232fb431c80639cdd6ed7ceec7fc169
sijokg <cijo_k_george@yahoo.co.in>
2013-05-14 12:46:30 +0530

    route for home#get

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Eco::Application.routes.draw do
+  get "home/index"
   devise_for :users
   root :to => "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
