c2384a15493e2556a24e2fbdf76937842e72fb7a
sijokg <cijo_k_george@yahoo.co.in>
2013-05-14 12:47:12 +0530

    sign in integration test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,28 +1,31 @@
 require 'spec_helper'
 
 feature 'Visitor sign in' do
+  let(:user) {create :user }
   scenario 'with valid email and password' do
-    sign_in FactoryGirl.create(:user)
-
-    expect(page).to have_content('Sign out')
+    sign_in user.email, user.password
+    page.should have_content("Sign out")
   end
 
   scenario 'with invalid email' do
-    sign_in FactoryGirl.create(:user, email: 'blah')
-
-    expect(page).to have_content('Sign in')
+    sign_in 'blah', user.password
+    page.should have_content('Invalid email')
   end
 
   scenario 'with blank password' do
-    sign_in FactoryGirl.create(:user, password: nil)
+    sign_in user.email, nil
+    expect(page).to have_content('Invalid email or password')
+  end
 
-    expect(page).to have_content('Sign in')
+  context "As administrator" do
+    let(:user) {FactoryGirl.create(:user, admin: true)}
+    scenario 'with valid email and password' do
+      sign_in user.email, user.password
+      page.should have_content("Sign out")
+      page.should have_content("Administration")
+    end
+
   end 
 
-  def sign_in(user)
-    visit sign_in_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Sign in'
-  end
+  
 end
