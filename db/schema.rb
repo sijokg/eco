52ff62af95e4c375319b6cbeb3193691806eece4
sijokg <cijo_k_george@yahoo.co.in>
2014-06-24 18:17:27 +0530

    change in schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130508110611) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: true do |t|
     t.string   "name",        null: false
     t.string   "title",       null: false
