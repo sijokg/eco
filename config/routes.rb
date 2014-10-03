258351ad950131ff0e9ae5f510c143ebbbfbcdde
Pramod G. Menon <pramodgmenon@gmail.com>
2014-10-03 16:51:30 +0530

    event scaffold  added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Eco::Application.routes.draw do
   get "home/index"
   devise_for :users
   root :to => "home#index"
+
+	resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
