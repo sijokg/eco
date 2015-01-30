4047302752817e0c6021743e3acc8ff9f5aa813c
sijokg <cijo_k_george@yahoo.co.in>
2015-01-30 14:35:36 +0530

    login by either username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
+  validates :username,  :presence => true, :uniqueness => { :case_sensitive => false }
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -7,5 +10,15 @@ class User < ActiveRecord::Base
 
   def is_admin?
     admin == true
+  end
+
+  #make either username or email authentication parameter
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
   end
 end
